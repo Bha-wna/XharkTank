44693d41299ba49d8bfdee32e14a7e29befc4a4f
bhawna29pragya <bhawna29pragya@gmail.com>
2022-12-01 21:57:54 +0530

    ME_BUILDOUT_XHARKTANK_MODULE_BASIC

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# shellcheck disable=SC1128
 # CRIO_SOLUTION_AND_STUB_START_MODULE_BASIC
 # This script must contain a series of commands to set up dependencies of your project to make it work.
 
@@ -11,13 +10,13 @@
 #           ./gradlew build
 
 # # Eg:- For Java Maven Project:
- 	        chmod +x mvnw
- 	        ./mvnw clean install compile test
+ 	        chmod +x ME_BUILDOUT_XHARKTANK_MODULE_BASIC
+ 	        ./ME_BUILDOUT_XHARKTANK_MODULE_BASIC clean install compile test
 
 # # Eg:- For Python Project;
 #           pip install -r requirements.txt
 
 # # Eg:- Run a shell script or bash commands to create tables or seed data into the database.
-          export PGPASSWORD="Admin@1324";psql -h localhost -U admin -p 5432 -d xharktank -f < XharkTank.sql
+          export PGPASSWORD="Admin@1324"; psql -h localhost -U admin -p 5432 -d xharktank -f < XharkTank.sql
 
 # CRIO_SOLUTION_AND_STUB_END_MODULE_BASIC
